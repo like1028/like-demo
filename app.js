25a362d56b5e5516c72224a8a907436d5326beee
like <978885880@qq.com>
2017-12-24 13:25:28 +0800

    提交智能

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ *@file     app.js
+ *@author   Like (978885880@qq.com)
+ *@date     2017/12/22
+ *@disc
+ */
+const Web3 = require('web3');
+// console.log(Web3);
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+// console.log(web3);
+// const solc = require('solc'); //solc编译器编译智能合约
+// // json interface describing the ABI for an Ethereum smart contract
+// let source = "contract test { function multiply(uint a) returns(uint d) { return a * 7; }}";
+// let compiled = solc.compile(source);
+// console.log(compiled);
+// let eth = web3.eth;
+// web3.eth.getAccounts(console.log); // eth_accounts dose not exist/ is not available
+web3.eth.defaultAccount = '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe'; // default "from"
+// web3.eth.sendTransaction();
+// web3.eth.call();
+var b = web3.eth.defaultBlock;
+console.log(b);
+var c = web3.eth.getCoinbase();
+console.log(c);
+var d = web3.eth.getAccounts();
+console.log(d);
---
+++ smartContract.js
@@ -0,0 +1,43 @@
+/**
+ *@fileName smartContract.js
+ *@author   Like (978885880@qq.com)
+ *@date     2017/12/23
+ *@disc
+ */
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const solc = require('solc');
+let source = "contract test { function multiply(uint a) returns(uint d) { return a * 7; }}";
+// console.log(solc);
+let compiled = solc.compile(source);
+let contract = 'test';
+let contractClass = ":" + contract;
+
+let abiDefinition = JSON.parse(compiled.contracts[contractClass].interface);
+// console.log("abiDefinition:"+abiDefinition);
+let Abi = compiled.contracts[contractClass].interface;
+// console.log("Abi:"+Abi);
+let Code = compiled.contracts[contractClass].bytecode;
+// console.log("Code:"+Code);
+let myContract = new web3.eth.Contract(abiDefinition);
+// console.log("myContract:"+myContract);
+
+myContract.deploy({ data: Code }).send({
+    from: '0xc469382f43936834ceaef1c3fa67e0f796406cd7',
+    gas: 1500000,
+    gasPrice: '30000000000000'
+}, function (error, transactionHash) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(transactionHash);
+//记得写个地址不然会不能调用
+    myContract.options.address = '0xabd403c1eee32aedb3bbce90061808774be13d5c';
+// multiply是合约中的方法
+    myContract.methods.multiply(12).call({ from: '0xabd403c1eee32aedb3bbce90061808774be13d5c' }).then(
+        function (result) {
+            console.log(result)
+            //正确结果应该是84
+        }
+    )
+});
