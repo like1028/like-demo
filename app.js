e4c04c0d2cefd721ec90def9117e34072ee9f34f
like <978885880@qq.com>
2017-12-27 14:54:47 +0800

    更新“ ; git commit -m 更新“

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 // console.log(web3);
 // const solc = require('solc'); //solc编译器编译智能合约
 // // json interface describing the ABI for an Ethereum smart contract
-// let source = "contract test { function multiply(uint a) returns(uint d) { return a * 7; }}";
+let source = "contract test { function multiply(uint a) returns(uint d) { return a * 7; }}";
 // let compiled = solc.compile(source);
 // console.log(compiled);
 // let eth = web3.eth;
@@ -18,9 +18,39 @@ const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 web3.eth.defaultAccount = '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe'; // default "from"
 // web3.eth.sendTransaction();
 // web3.eth.call();
-var b = web3.eth.defaultBlock;
-console.log(b);
-var c = web3.eth.getCoinbase();
-console.log(c);
-var d = web3.eth.getAccounts();
-console.log(d);
+// var b = web3.eth.defaultBlock;
+// console.log(b);
+// var c = web3.eth.getCoinbase();
+// console.log(c);
+// var d = web3.eth.getAccounts();
+// console.log(d);
+// web3.eth.compile.solidity(source).then(console.log);
+// web3.eth.subscribe('newBlockHeaders');
+// new web3.eth.Contract(jsonInterface);
+// var Accounts = web3.eth.accounts;
+// console.log(Accounts);
+// var account = web3.eth.accounts.create();
+// console.log(account);
+// var keyStore = web3.eth.accounts.encrypt(privateKey, password);
+web3.eth.accounts.decrypt(keystoreJsonV3, password);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
